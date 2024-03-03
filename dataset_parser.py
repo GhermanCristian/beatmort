5bd5bfdb4a27f25bb7a69160f9b616265d9f4d7a
GhermanCristian <gherman_cristi10@yahoo.com>
2024-03-03 21:48:12 +0200

    Created DatasetParser ABC

---
+++ dataset_parser.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class DatasetParser(ABC):
+    def _save_to_txt_file(self, notes: list[str], file_name: str) -> None:
+        with open(file_name, "a+") as f:
+            line = " ".join(notes)
+            f.write(line + "\n")
+
+    @abstractmethod
+    def run(self) -> None:
+        pass
