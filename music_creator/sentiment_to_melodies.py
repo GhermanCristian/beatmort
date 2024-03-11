3bca6715b5456ef830bce7eacd0ececca3f2309d
GhermanCristian <gherman_cristi10@yahoo.com>
2024-03-11 22:50:43 +0200

    Added FEAR to SentimentToMelodies

--- music_creator/sentiment_to_melodies.py
+++ music_creator/sentiment_to_melodies.py
@@ -43,37 +43,53 @@ class SentimentToMelodies:
             instrument.Tenor(),
             instrument.Baritone(),
             instrument.Bass(),
+        ],
+        Sentiment.FEAR: [
+            instrument.Piano(),
+            instrument.Sampler(),
+            instrument.ElectricPiano(),
+            instrument.Organ(),
+            instrument.PipeOrgan(),
+            instrument.StringInstrument(),
+            instrument.AcousticBass(),
+            instrument.Ukulele(),
+            instrument.Koto(),
+            instrument.Tuba(),
         ]
     }
 
     MEASURE_LENGTHS = {
-        Sentiment.JOY: [2, 4]
+        Sentiment.JOY: [2, 4],
+        Sentiment.FEAR: [1, 2, 4],
     }
 
     DURATIONS = {
-        Sentiment.JOY: [0.25, 0.5]
+        Sentiment.JOY: [0.25, 0.5],
+        Sentiment.FEAR: [1.5, 2],
     }
 
     OCTAVE_OFFSETS = {
-        Sentiment.JOY: [1, 1.5, 2]
+        Sentiment.JOY: [1, 1.5, 2],
+        Sentiment.FEAR: [-2.5, -2, -1.5],
     }
 
     PAUSE_DURATIONS = {
-        Sentiment.JOY: [0.25]
+        Sentiment.JOY: [0.25, 0.25],
+        Sentiment.FEAR: [1.5, 2],
     }
 
     def run(self, sentiment: Sentiment) -> list[MelodyInfo]:
-        n_melodies = 2
+        n_melodies = 2  # TODO - n_melodies for every sentiment
         instruments: list[instrument.Instrument] = random.sample(self.INSTRUMENTS[sentiment], n_melodies)
         measure_lengths: list[int] = random.sample(self.MEASURE_LENGTHS[sentiment], n_melodies)
         durations: list[float] = random.sample(self.DURATIONS[sentiment], n_melodies)
         octave_offsets: list[float] = random.sample(self.OCTAVE_OFFSETS[sentiment], n_melodies)
-        pause_durations: list[float] = random.sample(self.PAUSE_DURATIONS[sentiment], counts=[n_melodies], k=n_melodies)
-        print(instruments)
-        print(measure_lengths)
-        print(durations)
-        print(octave_offsets)
-        print(pause_durations)
+        pause_durations: list[float] = random.sample(self.PAUSE_DURATIONS[sentiment], n_melodies)
+        print("instruments", instruments)
+        print("measure_lengths", measure_lengths)
+        print("durations", durations)
+        print("octave_offsets", octave_offsets)
+        print("pause_durations", pause_durations)
         return [
             MelodyInfo(instruments[0], measure_lengths[0], durations[0], 0, octave_offsets[0], 1, pause_durations[0])
         ] + [
