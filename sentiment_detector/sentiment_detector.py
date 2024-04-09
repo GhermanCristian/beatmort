3e0fd2fba15a4183254513817450c49c36e39071
GhermanCristian <gherman_cristi10@yahoo.com>
2024-04-09 22:18:43 +0300

    Created SentimentDetector class

--- sentiment_detector/sentiment_detector.py
+++ sentiment_detector/sentiment_detector.py
@@ -4,40 +4,43 @@ import nltk
 
 from sentiment import Sentiment
 
-nltk.download("wordnet", download_dir="Corpora")
-nltk.data.path.append("Corpora")
 
-brown_ic = wordnet_ic.ic("ic-brown.dat")
+class SentimentDetector:
+    CORPORA_DIR = "Corpora"
+
+    def __init__(self) -> None:
+        nltk.download("wordnet", download_dir=self.CORPORA_DIR)
+        nltk.data.path.append(self.CORPORA_DIR)
+        self._brown_ic = wordnet_ic.ic("ic-brown.dat")
+
+    def _get_synset(self, word: str):
+        try:
+            synset = wn.synsets(word)[0]
+            # TODO - force everything to be noun ? or find adj/verb similar to the sentiments
+        except KeyError:
+            synset = wn.morphy(word)
+        return synset
+
+    def _similarity_score(self, s1, s2) -> float:
+        return (
+            s1.path_similarity(s2)
+            # + s1.lch_similarity(s2) * 0.1  # TODO - find better coeff for all these
+            + s1.wup_similarity(s2)
+            # + s1.res_similarity(s2, brown_ic) * 0.025
+            # + s1.jcn_similarity(s2, brown_ic) * 1.0
+            + s1.lin_similarity(s2, self._brown_ic)
+        ) / 3.0
+
+    def get_closest_sentiment(self, word: str) -> Sentiment:
+        word_synset = self._get_synset(word)
+        scores = {
+            s: self._similarity_score(word_synset, wn.synset(f"{s.value}.n.01")) for s in Sentiment
+        }
+        # TODO - remove neutral, select it if no value is > threshold || too many values close to the max one
+        for k, v in scores.items():
+            print(k, v)
+        return max(scores, key=lambda k: scores[k])
 
 
-def get_synset(word: str):
-    try:
-        synset = wn.synsets(word)[0]
-        # TODO - force everything to be noun ? or find adj/verb similar to the sentiments
-    except KeyError:
-        synset = wn.morphy(word)
-    return synset
-
-
-def similarity_score(s1, s2) -> float:
-    return (
-        s1.path_similarity(s2)
-        # + s1.lch_similarity(s2) * 0.1  # TODO - find better coeff for all these
-        + s1.wup_similarity(s2)
-        # + s1.res_similarity(s2, brown_ic) * 0.025
-        # + s1.jcn_similarity(s2, brown_ic) * 1.0
-        + s1.lin_similarity(s2, brown_ic)
-    ) / 3.0
-
-
-def get_closest_sentiment(word: str) -> Sentiment:
-    word_synset = get_synset(word)
-    scores = {s: similarity_score(word_synset, wn.synset(f"{s.value}.n.01")) for s in Sentiment}
-    # TODO - remove neutral, select it if no value is > threshold || too many values close to the max one
-    for k, v in scores.items():
-        print(k, v)
-    return max(scores, key=lambda k: scores[k])
-
-
-w = "regret"
-print(f"Closest sentiment to '{w}' is {get_closest_sentiment(w)}")
+w = "football"
+print(f"Closest sentiment to '{w}' is {SentimentDetector().get_closest_sentiment(w)}")
