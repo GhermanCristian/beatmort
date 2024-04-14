6561824d4500da72ab1c6b6d9d3667ef4dad9aaa
GhermanCristian <gherman_cristi10@yahoo.com>
2024-04-14 18:48:52 +0300

    Removed class_names param from SentimentDetector

--- sentiment_detector/sentiment_detector.py
+++ sentiment_detector/sentiment_detector.py
@@ -6,14 +6,10 @@ from tensorflow.keras.models import Model
 
 
 class SentimentDetector:
-    def __init__(
-        self, tokenizer: Tokenizer, model: Model, max_seq_len: int, class_names: list[str]
-    ) -> None:
+    def __init__(self, tokenizer: Tokenizer, model: Model, max_seq_len: int) -> None:
         self._tokenizer = tokenizer
         self._model = model
         self._max_seq_len = max_seq_len
-        self._class_names = class_names
-        # TODO - replace class names with iteration over Sentiment (order might be different => maybe retrain)
 
     def run(self, prompt: str) -> Sentiment:
         seq = self._tokenizer.texts_to_sequences(prompt)
@@ -21,6 +17,6 @@ class SentimentDetector:
 
         prediction = self._model.predict(padded)
 
-        sentiment = self._class_names[np.argmax(prediction)]
+        sentiment = Sentiment.class_names()[np.argmax(prediction)]
         print(f"Prompt = {prompt}; sentiment = {sentiment}")
         return Sentiment[sentiment]
