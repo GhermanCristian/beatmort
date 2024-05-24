46c390ebab336c7c6065541d4a85aa53a7392382
GhermanCristian <gherman_cristi10@yahoo.com>
2024-05-24 21:34:58 +0300

    Cleanup in Utils

--- utils.py
+++ utils.py
@@ -9,22 +9,24 @@ class Utils:
     EMBEDDINGS_DIR = "embeddings"
     EMBEDDING_MATRIX_PATH = Path(f"{EMBEDDINGS_DIR}/{EMBEDDING_MATRIX_NAME}")
 
-    def download_embedding_matrix(self) -> None:
+    @staticmethod
+    def download_embedding_matrix() -> None:
         urllib.request.urlretrieve(
-            f"https://dl.fbaipublicfiles.com/fasttext/vectors-english/{self.EMBEDDING_MATRIX_NAME}.zip",
-            f"{self.EMBEDDING_MATRIX_PATH}.zip",
+            f"https://dl.fbaipublicfiles.com/fasttext/vectors-english/{Utils.EMBEDDING_MATRIX_NAME}.zip",
+            f"{Utils.EMBEDDING_MATRIX_PATH}.zip",
         )
-        with zipfile.ZipFile(f"{self.EMBEDDING_MATRIX_PATH}.zip", "r") as zip_ref:
-            zip_ref.extractall(self.EMBEDDINGS_DIR)
+        with zipfile.ZipFile(f"{Utils.EMBEDDING_MATRIX_PATH}.zip", "r") as zip_ref:
+            zip_ref.extractall(Utils.EMBEDDINGS_DIR)
 
-        Path(f"{self.EMBEDDING_MATRIX_PATH}.zip").unlink()
+        Path(f"{Utils.EMBEDDING_MATRIX_PATH}.zip").unlink()
 
+    @staticmethod
     def load_embedding_matrix(
-        self, vocabulary_size: int, n_dims_embedding: int, word_index: dict[str, int]
+        vocabulary_size: int, n_dims_embedding: int, word_index: dict[str, int]
     ) -> list[np.ndarray]:
         embedding_matrix = np.zeros((vocabulary_size, n_dims_embedding))
         with open(
-            self.EMBEDDING_MATRIX_PATH, "r", encoding="utf-8", newline="\n", errors="ignore"
+            Utils.EMBEDDING_MATRIX_PATH, "r", encoding="utf-8", newline="\n", errors="ignore"
         ) as f:
             for line in f:
                 word, *vector = line.split()
