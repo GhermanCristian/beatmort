d83f559257a3f9f8c752702c34ddc8da9e9b872b
GhermanCristian <gherman_cristi10@yahoo.com>
2024-05-25 16:21:22 +0300

    Cleanup in DataLoader (music)

--- src/train/music_creator/data_loader.py
+++ src/train/music_creator/data_loader.py
@@ -18,8 +18,7 @@ class DataContainer:
 
 
 class DataLoader:
-    def __init__(self, feature_length: int, lim: int) -> None:
-        self._feature_length = feature_length
+    def __init__(self, lim: int) -> None:
         self._lim = lim
 
     def get_notes_from_txt(self, txt_path: str) -> list[str]:
@@ -43,14 +42,14 @@ class DataLoader:
         filtered_notes = [n for n in all_notes if count[n] >= self._lim]
         return filtered_notes
 
-    def create_indices(self, filtered_notes: list[str]) -> tuple[dict[int, str], dict[str, int]]:
+    def create_indices(self, filtered_notes: list[str]) -> dict[int, str]:
         unique_notes = sorted(list(set(filtered_notes)))
 
         index = {note: ind for ind, note in enumerate(unique_notes)}
         reverse_index = dict(enumerate(unique_notes))
         with open(Constants.MUSIC_REVERSE_INDEX_PATH, "wb") as reverse_index_path:
             pickle.dump(reverse_index, reverse_index_path)
-        return index, reverse_index
+        return index
 
     @staticmethod
     def save_seed_to_disk(data_container: DataContainer) -> None:
@@ -62,18 +61,18 @@ class DataLoader:
     ) -> DataContainer:
         features = []
         targets = []
-        for i in range(0, len(filtered_notes) - self._feature_length):
-            feature = filtered_notes[i : i + self._feature_length]
-            target = filtered_notes[i + self._feature_length]
+        for i in range(0, len(filtered_notes) - Constants.MUSIC_FEATURE_LENGTH):
+            feature = filtered_notes[i : i + Constants.MUSIC_FEATURE_LENGTH]
+            target = filtered_notes[i + Constants.MUSIC_FEATURE_LENGTH]
             features.append([index[j] for j in feature])
             targets.append(index[target])
 
         n_datapoints = len(targets)
 
         vocab_size = len(index)
-        x = (np.reshape(features, (n_datapoints, self._feature_length, 1), order="C")) / float(
-            vocab_size
-        )
+        x = (
+            np.reshape(features, (n_datapoints, Constants.MUSIC_FEATURE_LENGTH, 1), order="C")
+        ) / float(vocab_size)
         y = tensorflow.keras.utils.to_categorical(targets)
         x_train, x_seed, y_train, y_seed = train_test_split(
             x, y, test_size=seed_size, random_state=42
