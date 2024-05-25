59cd0da63fc495ebe339397ad91b4399f778063c
GhermanCristian <gherman_cristi10@yahoo.com>
2024-05-25 18:58:15 +0300

    Created Predictor class

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -12,48 +12,50 @@ from predict.sentiment_classifier.sentiment_classifier import SentimentClassifie
 from tensorflow.keras.models import load_model
 
 
-def _classify_sentiment(prompt: str) -> Sentiment:
-    with open(Constants.SENTIMENT_TOKENIZER_PATH, "rb") as tokenizer_path:
-        tokenizer = pickle.load(tokenizer_path)
-    model = load_model(Constants.SENTIMENT_MODEL_PATH)
+class Predictor:
+    def load_artifacts(self) -> None:
+        with open(Constants.SENTIMENT_TOKENIZER_PATH, "rb") as tokenizer_path:
+            self._sentiment_tokenizer = pickle.load(tokenizer_path)
+        self._sentiment_model = load_model(Constants.SENTIMENT_MODEL_PATH)
 
-    sentiment_classifier = SentimentClassifier(tokenizer, model, Constants.SENTIMENT_MAX_SEQ_LEN)
-    sentiment: Sentiment = sentiment_classifier.run(prompt)
-    return sentiment
+        with open(Constants.MUSIC_REVERSE_INDEX_PATH, "rb") as reverse_index_path:
+            self._music_reverse_index = pickle.load(reverse_index_path)
+        with open(Constants.MUSIC_SEED_PATH, "rb") as music_seed_path:
+            self._music_seed = np.load(music_seed_path)
+        self._music_model = load_model(Constants.MUSIC_MODEL_PATH)
 
+        with open(Constants.LYRICS_TOKENIZER_PATH, "rb") as tokenizer_path:
+            self._lyrics_tokenizer = pickle.load(tokenizer_path)
+        self._lyrics_model = load_model(Constants.LYRICS_MODEL_PATH)
 
-def _generate_music(sentiment: Sentiment) -> None:
-    with open(Constants.MUSIC_REVERSE_INDEX_PATH, "rb") as reverse_index_path:
-        reverse_index = pickle.load(reverse_index_path)
-    with open(Constants.MUSIC_SEED_PATH, "rb") as music_seed_path:
-        seed = np.load(music_seed_path)
-    model = load_model(Constants.MUSIC_MODEL_PATH)
+    def _classify_sentiment(self, prompt: str) -> Sentiment:
+        sentiment_classifier = SentimentClassifier(
+            self._sentiment_tokenizer, self._sentiment_model, Constants.SENTIMENT_MAX_SEQ_LEN
+        )
+        sentiment: Sentiment = sentiment_classifier.run(prompt)
+        return sentiment
 
-    music_creator = MusicCreator(model, seed, reverse_index)
-    melodies = SentimentToMelodies().run(sentiment)
-    main_score = music_creator.run(128, melodies)
-    SongSaver.save_song_to_disk(
-        main_score,
-        "..\\Outputs/test",
-        Path("C:\Program Files\MuseScore 4\\bin\MuseScore4.exe"),
-        Path("..\\FluidSynth\\fluidsynth.exe"),
-        Path("..\\FluidSynth\\GeneralUser GS v1.471.sf2"),
-    )
+    def _generate_music(self, sentiment: Sentiment) -> None:
+        music_creator = MusicCreator(self._music_model, self._music_seed, self._music_reverse_index)
+        melodies = SentimentToMelodies().run(sentiment)
+        main_score = music_creator.run(128, melodies)
+        SongSaver.save_song_to_disk(
+            main_score,
+            "..\\Outputs/test",
+            Path("C:\Program Files\MuseScore 4\\bin\MuseScore4.exe"),
+            Path("..\\FluidSynth\\fluidsynth.exe"),
+            Path("..\\FluidSynth\\GeneralUser GS v1.471.sf2"),
+        )
 
+    def _generate_lyrics(self, sentiment: Sentiment, n_verses: int) -> list[str]:
+        lyric_generator = LyricGenerator(
+            Constants.LYRICS_MAX_SEQ_LEN, self._lyrics_tokenizer, self._lyrics_model
+        )
+        lyrics = lyric_generator.run(n_verses, sentiment)
+        return lyrics
 
-def _generate_lyrics(sentiment: Sentiment, n_verses: int) -> list[str]:
-    with open(Constants.LYRICS_TOKENIZER_PATH, "rb") as tokenizer_path:
-        tokenizer = pickle.load(tokenizer_path)
-    model = load_model(Constants.LYRICS_MODEL_PATH)
-
-    lyric_generator = LyricGenerator(Constants.LYRICS_MAX_SEQ_LEN, tokenizer, model)
-    lyrics = lyric_generator.run(n_verses, sentiment)
-    return lyrics
-
-
-def run(prompt: str) -> None:
-    sentiment = _classify_sentiment(prompt)
-    _generate_music(sentiment)
-    lyrics = _generate_lyrics(sentiment, 16)
-    for l in lyrics:
-        print(l)
+    def run(self, prompt: str) -> list[str]:
+        sentiment = self._classify_sentiment(prompt)
+        self._generate_music(sentiment)
+        lyrics = self._generate_lyrics(sentiment, 16)
+        return lyrics
