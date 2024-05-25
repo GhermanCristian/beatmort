9d180097b3438dc8d1e11f5561a565e8359ad8af
GhermanCristian <gherman_cristi10@yahoo.com>
2024-05-25 19:12:24 +0300

    Artifact loading is now done in a separate thread to speed up start times

--- src/ui/GUI.py
+++ src/ui/GUI.py
@@ -1,3 +1,4 @@
+import threading
 from tkinter import Tk
 import tkinter as tk
 from typing import Final
@@ -8,7 +9,6 @@ from predict.predict import Predictor
 class GUI:
     def __init__(self) -> None:
         self._predictor = Predictor()
-        self._predictor.load_artifacts()  # TODO - do in separate thread
 
     def __create_main_window(self) -> Tk:
         WINDOW_TITLE: Final[str] = "apptitle"
@@ -31,11 +31,13 @@ class GUI:
         tk.Label(main_window, text="What's on your mind ?").grid(row=0)
         user_input = tk.Entry(main_window)
         user_input.grid(row=0, column=1)
-        tk.Button(main_window, text="Submit", command=lambda: self._on_submit_action(user_input)).grid(
-            row=3, column=1, sticky=tk.W, pady=4
-        )
+        tk.Button(
+            main_window, text="Submit", command=lambda: self._on_submit_action(user_input)
+        ).grid(row=3, column=1, sticky=tk.W, pady=4)
 
     def run(self) -> None:
+        thread = threading.Thread(target=self._predictor.load_artifacts)
+        thread.start()
         main_window: Tk = self.__create_main_window()
         self._create_user_input_section(main_window)
         main_window.mainloop()
