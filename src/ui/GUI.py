e39374898311c164aa62b68a30a0ad8243e6ca8a
GhermanCristian <gherman_cristi10@yahoo.com>
2024-05-26 17:51:20 +0300

    Design improvements; can now play/stop songs

--- src/ui/GUI.py
+++ src/ui/GUI.py
@@ -1,7 +1,7 @@
 import threading
 from tkinter import Tk, Scale
 import tkinter as tk
-from typing import Final
+from typing import Final, Optional
 
 from predict.predict import Predictor
 import simpleaudio as sa
@@ -15,12 +15,12 @@ class GUI:
         self._predictor = Predictor()
         self._main_window: Tk = self.__create_main_window()
         self._user_input: tk.Entry = self._create_user_input_section()
-        submit_button = tk.Button(self._main_window, text="Submit", command=self._on_submit_action)
-        submit_button.pack()
         self._n_verses_scale: tk.Scale = Scale(
-            self._main_window, from_=2, to=32, length=200, width=25
+            self._main_window, from_=2, to=32, length=200, width=25, orient="horizontal"
         )
         self._n_verses_scale.pack()
+        submit_button = tk.Button(self._main_window, text="Submit", command=self._on_submit_action)
+        submit_button.pack()
         self._lyrics_label: tk.Label = tk.Label(
             self._main_window,
             text="Lyrics will end up here",
@@ -31,6 +31,7 @@ class GUI:
         self._lyrics_label.pack()
 
         self._is_playing: bool = False
+        self._song: Optional[sa.PlayObject] = None
         self._play_song_button = tk.Button(
             self._main_window, text="Play", command=self._change_playing_state
         )
@@ -38,8 +39,8 @@ class GUI:
 
     def __create_main_window(self) -> Tk:
         WINDOW_TITLE: Final[str] = "apptitle"
-        MIN_WINDOW_WIDTH_IN_PIXELS: Final[int] = 1100
-        MIN_WINDOW_HEIGHT_IN_PIXELS: Final[int] = 480
+        MIN_WINDOW_WIDTH_IN_PIXELS: Final[int] = 720
+        MIN_WINDOW_HEIGHT_IN_PIXELS: Final[int] = 720
 
         main_window: Tk = Tk()
         main_window.title(WINDOW_TITLE)
@@ -60,14 +61,16 @@ class GUI:
         self._is_playing = not self._is_playing
         if self._is_playing:
             wave_obj = sa.WaveObject.from_wave_file("../Outputs/test.wav")
-            wave_obj.play()
+            self._song = wave_obj.play()
+            self._play_song_button.config(text="Stop")
         else:
-            pass
+            self._song.stop()
+            self._play_song_button.config(text="Play")
 
     def _create_user_input_section(self) -> None:
         label = tk.Label(self._main_window, text="How are you feeling today ?", font=self.FONT)
         label.pack()
-        user_input = tk.Entry(self._main_window, width=100, font=self.FONT_INPUT)
+        user_input = tk.Entry(self._main_window, width=60, font=self.FONT_INPUT)
         user_input.pack()
         return user_input
 
