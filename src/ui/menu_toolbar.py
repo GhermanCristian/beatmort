0ef4727a016a9a71f285c3a5ea099bf11f284c73
GhermanCristian <gherman_cristi10@yahoo.com>
2024-06-05 18:36:54 +0300

    Adapted menu toolbar

--- src/ui/menu_toolbar.py
+++ src/ui/menu_toolbar.py
@@ -18,18 +18,13 @@ class MenuToolbar:
     def _about_command(self) -> None:
         pass  # AboutUBTorrentWindow(self._main_window)
 
-    def _send_feedback_command(self) -> None:
-        pass  # SendFeedbackWindow(self._main_window)
-
     def create(self) -> Menu:
         SETTINGS_COMMAND_LABEL: Final[str] = "Settings"
         HELP_COMMAND_LABEL: Final[str] = "Help"
-        ABOUT_APPNAME_COMMAND_LABEL: Final[str] = "About appname"
-        SEND_FEEDBACK_COMMAND_LABEL: Final[str] = "Send feedback"
+        ABOUT_MOODSIC_COMMAND_LABEL: Final[str] = "About Moodsic"
 
         menu_bar: Menu = Menu(self._main_window)
         menu_bar.add_command(label=SETTINGS_COMMAND_LABEL, command=self._settings_command)
         menu_bar.add_command(label=HELP_COMMAND_LABEL, command=self._help_command)
-        menu_bar.add_command(label=ABOUT_APPNAME_COMMAND_LABEL, command=self._about_command)
-        menu_bar.add_command(label=SEND_FEEDBACK_COMMAND_LABEL, command=self._send_feedback_command)
+        menu_bar.add_command(label=ABOUT_MOODSIC_COMMAND_LABEL, command=self._about_command)
         return menu_bar
